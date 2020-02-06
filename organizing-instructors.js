239e133926d8059725a59959de025ea2c3c17efc
cphung1 <cphung97@gmail.com>
2020-02-06 23:34:40 +0000

    inital commit

---
+++ organizing-instructors.js
@@ -0,0 +1,31 @@
+const organizeInstructors = (instructors) => {
+  let outputObject = {};
+  for (const val in instructors) {
+    outputObject[instructors[val].course] = [];
+  }
+
+  for (const val in instructors) {
+    if(instructors[val].course) {
+      outputObject[instructors[val].course].push(instructors[val].name)
+    } else {
+      outputObject[instructors[val].course] = instructors[val].name
+    }
+  }
+  return outputObject;
+};
+
+
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
